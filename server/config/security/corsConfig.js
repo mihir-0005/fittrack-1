b8f3585556c3914184cc59d568c393f7bbb60d28
mihir-0005 <110338461+mihir-0005@users.noreply.github.com>
2024-12-14 17:31:07 +0530

    -

--- server/config/security/corsConfig.js
+++ server/config/security/corsConfig.js
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const corsConfig = {
-  origin: ['https://fittrack-app.netlify.app', 'http://localhost:5173'],
+  origin: ['https://flourishing-basbousa-b3c0d9.netlify.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
