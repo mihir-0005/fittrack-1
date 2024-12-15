1016e45cbf2fa5d05cffaa047936dba8fd23327f
mihir-0005 <110338461+mihir-0005@users.noreply.github.com>
2024-12-15 21:36:53 +0530

    -

--- src/services/challengeService.js
+++ src/services/challengeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://fittrack-1-yefe.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL  ;
 
 export const createChallenge = async (challengeData) => {
   try {
